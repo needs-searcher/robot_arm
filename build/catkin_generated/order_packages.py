672282861e75ac924edeb1400bdb5325e429bdf1
needs-searcher <jh03130@gmail.com>
2020-11-06 01:43:45 +0900

    rough modeling

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/juhyeong/mm_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/juhyeong/catkin_ws/devel;/home/juhyeong/ws_moveit/devel;/opt/ros/melodic'.split(';') if '/home/juhyeong/catkin_ws/devel;/home/juhyeong/ws_moveit/devel;/opt/ros/melodic' != '' else []
